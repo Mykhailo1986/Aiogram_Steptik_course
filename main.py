211b7e3d460dc62b47bff447fb37691204f1437f
Misha <i_am_misha@i.ua>
2023-05-18 10:58:41 +0300

    Эхо-бот для фото

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from config import TOKEN
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
 # Замість BOT TOKEN HERE потрібно вставити токен вашого бота, отриманого від @BotFather
 API_TOKEN: str = TOKEN
@@ -23,6 +25,11 @@ async def process_start_command(message: Message):
 async def process_help_command(message: Message):
     await message.answer('Напиши мені щось і відповім тобі твоє повідомлення')
 
+# Цей хендлер буде викликаний на відправку фото
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
 
 # Цей хендлер буде викликаний на будь-які ваші текстові повідомлення,
 # окрім команд "/start" і "/help"
@@ -39,6 +46,8 @@ async def send_echo(message: Message):
 # Реєструємо хендлери
 dp.message.register(process_start_command, Command(commands=["start"]))
 dp.message.register(process_help_command, Command(commands=['help']))
+# dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+dp.message.register(send_photo_echo, F.photo) # коротший вид запису
 dp.message.register(send_echo)
 
 if __name__ == '__main__':
