2edffb09054cf732173e2ad7bfb41fdf2b7e6ee7
Misha <i_am_misha@i.ua>
2023-05-28 18:52:09 +0300

    learned Relative imports

--- 8.1 Moduls & Packs/Relative import/main.py
+++ 8.1 Moduls & Packs/Relative import/main.py
@@ -1,7 +1,15 @@
-print('Это основной файл relative_import main.py, его имя в процессе выполнения программы:', __name__)
+# print('Это основной файл relative_import main.py, его имя в процессе выполнения программы:', __name__)
 
 
-from pack_1.file_11 import result
+# from pack_1.file_11 import result
 
 
-print('result =', result)
+# print('result =', result)
+
+print('Это основной файл main.py, его имя в процессе выполнения программы:', __name__)
+
+
+from pack_2.pack_21.file_211 import r
+
+
+print('r =', r)
