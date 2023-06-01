8415ce53831f6d1ae90d024111ebbfcda29a6400
Misha <i_am_misha@i.ua>
2023-06-01 21:12:33 +0300

    learned routers

---
+++ 8.1 Moduls & Packs/Initialization  Packs/main.py
@@ -0,0 +1,26 @@
+# print('Это основной файл main.py, его имя в процессе выполнения программы:', __name__)
+
+
+# # import pack_1
+# import pack_2
+# # from pack_2 import pack_21
+
+
+# print(dir())
+# # print(dir(pack_1))
+# print(dir(pack_2))
+# # print(dir(pack_21))
+
+# print('Это основной файл main.py, его имя в процессе выполнения программы:', __name__)
+
+
+# import pack_1
+
+
+# print(dir())
+# print(dir(pack_1))
+
+from pack_1.file_11 import func_1
+from pack_1.file_12 import a
+
+print(func_1(a))
---
+++ 8.1 Moduls & Packs/Initialization  Packs/pack_1/__init__.py
@@ -0,0 +1,5 @@
+# print('Это файл __init__.py пакета pack_1, его имя во время выполнения программы:', __name__)
+
+
+# from .file_11 import *
+# from . import file_12
