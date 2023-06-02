0581872dc6046c1f99aed07e998adcbadf8ea07f
Misha <i_am_misha@i.ua>
2023-06-02 06:54:44 +0300

    create 3x3 reyboars using generators

--- 9.2 Casual Buttons/button_location.py
+++ 9.2 Casual Buttons/button_location.py
@@ -16,21 +16,12 @@ bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 
 # Создаем объекты кнопок
-button_1: KeyboardButton = KeyboardButton(text='Кнопка 1')
-button_2: KeyboardButton = KeyboardButton(text='Кнопка 2')
-button_3: KeyboardButton = KeyboardButton(text='Кнопка 3')
-button_4: KeyboardButton = KeyboardButton(text='Кнопка 4')
-button_5: KeyboardButton = KeyboardButton(text='Кнопка 5')
-button_6: KeyboardButton = KeyboardButton(text='Кнопка 6')
-button_7: KeyboardButton = KeyboardButton(text='Кнопка 7')
-button_8: KeyboardButton = KeyboardButton(text='Кнопка 8')
-button_9: KeyboardButton = KeyboardButton(text='Кнопка 9')
+keyboard: list[list[KeyboardButton]] = [[KeyboardButton(
+    text=f'Кнопка {j * 3 + i}') for i in range(1, 4)] for j in range(3)]
 
 # Создаем объект клавиатуры, добавляя в него кнопки
 my_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-                        keyboard=[[button_1, button_2, button_3],
-                                  [button_4, button_5, button_6],
-                                  [button_7, button_8, button_9]],
+                        keyboard=keyboard,
                         resize_keyboard=True)
 
 # Этот хэндлер будет срабатывать на команду "/start"
